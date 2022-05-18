3d0231d4f9c8fa210a87f8d750ed5e5d4e05bd00
0xMett <mattiafailla@hotmail.it>
2022-05-18 18:46:33 +0200

    Add quick install script

---
+++ quick-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download the node-creator script
+wget https://raw.githubusercontent.com/JellyPool/meter-node-creator/launch_script/node-creator
+
+# Make it executable
+chmod a+x ./node-creator
+
+# @reboot start script
+homedir=$( getent passwd "$USER" | cut -d: -f6 )
+crontab -l > file; echo "@reboot ${homedir}/node-creator" >> file; crontab file; rm file
+
+# Run first install script
+./node-creator
+
