f15618a5a27f25e9a2037d4572d01d141593c2bb
0xMett <mattiafailla@hotmail.it>
2022-06-07 15:57:29 +0000

    Improve wget download location

--- quick-start.sh
+++ quick-start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# Export user home directory
+homedir=$( getent passwd "$USER" | cut -d: -f6 )
+
 # Download the node-creator script
-wget https://raw.githubusercontent.com/JellyPool/meter-node-creator/launch_script/node-creator
+wget https://raw.githubusercontent.com/JellyPool/meter-node-creator/launch_script/node-creator -P ${homedir}
 
 # Make it executable
 chmod a+x ./node-creator
 
 # @reboot start script
-homedir=$( getent passwd "$USER" | cut -d: -f6 )
 crontab -l > file; echo "@reboot ${homedir}/node-creator -dwy >> ${homedir}/node-creator.log 2>&1" >> file; crontab file; rm file
 
 # Run first install script
