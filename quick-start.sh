bb35b30fc31127fc9eda065ea7a269af5456a1f5
0xMett <mattiafailla@hotmail.it>
2022-06-09 03:14:40 +0200

    Improve node-creator location

--- quick-start.sh
+++ quick-start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# Export user home directory
+homedir=$( getent passwd "$USER" | cut -d: -f6 )
+
 # Download the node-creator script
-wget https://raw.githubusercontent.com/JellyPool/meter-node-creator/production/quick-start.sh
+wget https://raw.githubusercontent.com/JellyPool/meter-node-creator/production/node-creator -P ${homedir}
 
 # Make it executable
 chmod a+x ./node-creator
-
-# Export user home directory
-homedir=$( getent passwd "$USER" | cut -d: -f6 )
 
 # @reboot start script
 crontab -l > file; echo "@reboot ${homedir}/node-creator -dwy >> ${homedir}/node-creator.log 2>&1" >> file; crontab file; rm file
